dc26f524b9c31bf88522f6c0a9876eb54797d1ad
kadennotermux <66335850+kadennotermux@users.noreply.github.com>
2021-11-06 19:55:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ const sleep = async (ms) => {
 }
 const starts = async (client = new WAConnection()) => {
   client.logger.level = "warn";
-  client.version = [2, 2142, 12];
+  client.version = [2, 2143, 3];
   client.browserDescription = ["GingerBot", "Chrome", "3.0"];
   console.log(banner.string);
   client.on("qr", () => {
